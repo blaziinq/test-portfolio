83dd3bda18a8c6ac55e2dbbdd2150cdb24126e82
blaziinq <blaziinq@hotmail.com>
2022-06-18 11:43:08 +0100

    project page + footer
    In the project page:
    A bunch of stuff that I have made/will make have been added in links that look weird in html
    The footer has been pretty much completed.

--- start_body/change_header_and_footer.js
+++ start_body/change_header_and_footer.js
@@ -12,7 +12,50 @@ function make_header() {
 
   document.getElementById("header").innerHTML = navbar;
 }
-function make_footer() {}
+function make_footer() {
+  footer = `
+  <p>
+    Well, this is the bottom of the page, but if you want, here are all the
+    pages: <br />
+    <a href="index.html">The Homepage</a> <br />
+    <a href="projects.html">The Projects Page</a> <br />
+    <a href="about.html">The About Page</a> <br />
+    <a href="contact.html">How to Contact Me</a> <br />
+    And here are both of the fonts that were used in this website. <br />
+    <a
+      href="https://fonts.google.com/specimen/Poppins"
+      target="_blank"
+      rel="noopener noreferrer">
+      Poppins - For most of the text
+    </a>
+    <br />
+    <a
+      href="https://fonts.google.com/specimen/Source+Code+Pro"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Source Code Pro - For the navbar / terminal</a
+    >
+    <br />
+    Finally, here are my socials. <br />
+    <a
+      href="https://www.youtube.com/channel/UCW3HXaz5-JIYztVTASZ348A"
+      target="_blank"
+      rel="noopener noreferrer"
+      >My Youtube Channel</a
+    >
+    <br />
+    <a
+      href="https://twitter.com/blaziinqMC"
+      target="_blank"
+      rel="noopener noreferrer"
+      >My Twitter</a
+    >
+    <br />
+    <a href="#" onclick="CopyDiscordTag(); return false">My Discord</a>
+  </p>`;
+
+  document.getElementById("footer").innerHTML = footer;
+}
 
 // cross browser way to add an event listener
 function addListener(event, obj, fn) {
@@ -26,3 +69,9 @@ addListener("load", window, function () {
   make_header();
   make_footer();
 });
+
+function CopyDiscordTag() {
+  var discord_tag = "blaziinq#0018";
+  navigator.clipboard.writeText(discord_tag);
+  alert("'" + discord_tag + "'" + " has been copied to your clipboard");
+}
